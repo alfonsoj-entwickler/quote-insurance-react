27db7f79047a5e5b0125ed1bd2677f88eb0915d3
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-06-16 19:33:34 +0200

    [TASK] add global constants

---
+++ src/constants/index.js
@@ -0,0 +1,320 @@
+export const MARKS = [
+  { id: 'b20d4c44', name: "VW", continent: 'europa' },
+  { id: 'b20d4f5a', name: "Mercedes-Benz", continent: 'europa' },
+  { id: 'b20d50ae', name: "BMW", continent: 'europa' },
+  { id: 'b20d51e4', name: "Audi", continent: 'europa' },
+  { id: 'b20d534c', name: "Opel", continent: 'europa' },
+  { id: 'b20d5694', name: "Skoda", continent: 'europa' },
+  { id: 'b20d57c0', name: "Ford", continent: 'usa' },
+  { id: 'b20d58ec', name: "SEAT", continent: 'europa' },
+  { id: 'b20d5a0e', name: "CUPRA", continent: 'europa' },
+  { id: 'b20d5b26', name: "Hyundai", continent: 'asia' },
+  { id: 'b20d5c48', name: "Renault", continent: 'europa' },
+  { id: 'b20d5eb4', name: "Fiat", continent: 'europa' },
+  { id: 'b20d5fd6', name: "Toyota", continent: 'asia' },
+  { id: 'b20d60ee', name: "KIA", continent: 'asia' },
+  { id: 'b20d621a', name: "Peugeot", continent: 'europa' },
+  { id: 'b20d64ea', name: "Volvo", continent: 'europa' },
+  { id: 'b20d66de', name: "MINI", continent: 'europa' },
+  { id: 'b20d6990', name: "Citroen", continent: 'europa' },
+  { id: 'b20d6bd4', name: "Dacia", continent: 'europa' },
+  { id: 'b20d7282', name: "Tesla", continent: 'usa' },
+  { id: 'b20d755c', name: "Mazda", continent: 'europa' },
+  { id: 'b20d767e', name: "Mitsubishi", continent: 'asia' },
+  { id: 'b20d77b4', name: "Porsche", continent: 'europa' },
+  { id: 'b20d78d6', name: "Suzuki", continent: 'asia' },
+  { id: 'b20d79ee', name: "Nissan", continent: 'asia' },
+  { id: 'b20d7e26', name: "smart", continent: 'europa' },
+  { id: 'b20d7f66', name: "Jeep", continent: 'europa' },
+  { id: 'b20d8092', name: "Land Rover", continent: 'europa' },
+  { id: 'b20d81aa', name: "Honda", continent: 'asia' },
+  { id: 'b20d82cc', name: "Subaru", continent: 'asia' },
+  { id: 'b20d859c', name: "Jaguar", continent: 'europa' },
+  { id: 'b20d86c8', name: "Lexus", continent: 'europa' },
+  { id: 'b20d87d6', name: "Alfa Romeo", continent: 'europa' },
+  { id: 'b20d890c', name: "Polestar", continent: 'europa' },
+  { id: 'b20d8a24', name: "DS Automobiles", continent: 'europa' },
+  { id: 'b20d8b46', name: "SsangYong", continent: 'asia' },
+  { id: 'b20d8d4e', name: "Lancia", continent: 'europa' },
+  { id: 'b20d8e7a', name: "MG", continent: 'usa' },
+  { id: 'b20d8f92', name: "Daihatsu", continent: 'asia' },
+  { id: 'b20d90b4', name: "Abarth", continent: 'europa' },
+  { id: 'b20d933e', name: "Alpine", continent: 'europa' },
+  { id: 'b20d9618', name: "Aston Martin", continent: 'europa' },
+  { id: 'b20d9762', name: "Bentley", continent: 'europa' },
+  { id: 'b20d987a', name: "ALPINA", continent: 'europa' },
+  { id: 'b20d99f6', name: "Bugatti", continent: 'europa' },
+  { id: 'b20d9b40', name: "Cadillac", continent: 'usa' },
+  { id: 'b20d9d48', name: "Chevrolet", continent: 'usa' },
+  { id: 'b20d9e6a', name: "Chrysler", continent: 'europa' },
+  { id: 'b20d9f8c', name: "Maxus", continent: 'usa' },
+  { id: 'b20da0a4', name: "Dodge", continent: 'usa' },
+  { id: 'b20da1d0', name: "e.GO Mobile", continent: 'europa' },
+  { id: 'b20da3d8', name: "Elaris", continent: 'europa' },
+  { id: 'b20da504', name: "Electric Brands", continent: 'europa' },
+  { id: 'b20da766', name: "Sono Motors", continent: 'europa' },
+  { id: 'b20da89c', name: "Ferrari", continent: 'europa' },
+  { id: 'b20da9b4', name: "Fisker", continent: 'europa' },
+  { id: 'b20daad6', name: "Genesis", continent: 'europa' },
+  { id: 'b20dabf8', name: "Infiniti", continent: 'europa' },
+  { id: 'b20dad6a', name: "Isuzu", continent: 'europa' },
+  { id: 'b20db0bc', name: "Lada", continent: 'europa' },
+  { id: 'b20db1f2', name: "Lamborghini", continent: 'europa' },
+  { id: 'b20db30a', name: "Lotus", continent: 'europa' },
+  { id: 'b20db418', name: "Lynk &amp; Co", continent: 'europa' },
+  { id: 'b20db53a', name: "Maserati", continent: 'europa' },
+  { id: 'b20db7f6', name: "McLaren", continent: 'europa' },
+  { id: 'b20db922', name: "Micro", continent: 'europa' },
+  { id: 'b20dba44', name: "Morgan", continent: 'europa' },
+  { id: 'b20dbc38', name: "Rolls-Royce", continent: 'europa' },
+  { id: 'b20dbdbe', name: "Aiways", continent: 'europa' },
+  { id: 'b20dbefe', name: "Lucid", continent: 'europa' },
+  { id: 'b20dc020', name: "VinFast", continent: 'europa' },
+  { id: 'b20dc2b4', name: "Aixam", continent: 'europa' },
+  { id: 'b20dc3d6', name: "Ligier", continent: 'europa' },
+  { id: 'b20dc4f8', name: "Microcar", continent: 'europa' },
+  { id: 'b20dc62e', name: "Casalini", continent: 'europa' },
+  { id: 'b20dc750', name: "ARI Motors", continent: 'europa' },
+  { id: 'b20dca34', name: "Estrima", continent: 'europa' },
+  { id: 'b20dcb60', name: "Ineos", continent: 'europa' },
+  { id: 'b20dcc78', name: "BYD", continent: 'europa' },
+  { id: 'b20dcd9a', name: "WEY", continent: 'europa' },
+  { id: 'b20dceb2', name: "ORA", continent: 'europa' },
+  { id: 'b20dd09c', name: "NIO", continent: 'europa' },
+  { id: 'b20dd1be', name: "Togg", continent: 'europa' },
+  { id: 'b20dd34e', name: "SERES", continent: 'europa' },
+  { id: 'b20dd470', name: "Daewoo", continent: 'asia' },
+  { id: 'b20dd6dc', name: "Proton", continent: 'usa' },
+  { id: 'b20dd7fe', name: "Rover", continent: 'europa' },
+  { id: 'b20dd916', name: "Saab", continent: 'europa' },
+  { id: 'b20dda2e', name: "Wiesmann", continent: 'europa' },
+  { id: 'b20ddb46', name: "Hyundai Precision", continent: 'asia' },
+  { id: 'b20ddd9e', name: "Maybach", continent: 'europa' },
+  { id: 'b20de1b8', name: "Hummer", continent: 'europa' },
+  { id: 'b20de33e', name: "Brilliance", continent: 'europa' },
+  { id: 'b20de46a', name: "Datsun", continent: 'europa' },
+  { id: 'b20de582', name: "Trabant", continent: 'europa' },
+  { id: 'b20de6ae', name: "Talbot", continent: 'europa' },
+  { id: 'b20de7d0', name: "Wartburg", continent: 'europa' },
+  { id: 'b20dea32', name: "NSU", continent: 'europa' },
+  { id: 'b20deb4a', name: "DKW", continent: 'europa' },
+  { id: 'b20dec80', name: "Auto Union", continent: 'europa' },
+  { id: 'b20dedfc', name: "DAF", continent: 'europa' },
+  { id: 'b20def1e', name: "Glas", continent: 'europa' },
+  { id: 'b20df036', name: "Borgward", continent: 'europa' },
+  { id: 'b20df2ca', name: "Yugo", continent: 'europa' },
+];
+
+const YEARMAX = new Date().getFullYear();
+export const YEARS = Array.from( new Array(20), (value, index) => YEARMAX - index );
+
+export const PLANS = [
+    { id: 1, name: 'Basic'},
+    { id: 2, name: 'Complete'}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
